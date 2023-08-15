a40c29c7490ec6fc558743b42970a44446d515a6
GhassanGillani <ghassangillani@gmail.com>
2023-08-15 17:11:06 -0400

    Added Employee CRUD and React binding

--- PHP-Backend/routes/api.php
+++ PHP-Backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+
+Route::get('/employees',[App\Http\Controllers\EmployeeController::class, 'index']);
+
+Route::post('/save',[App\Http\Controllers\EmployeeController::class, 'store']);
+
+Route::put('/update/{id}',[App\Http\Controllers\EmployeeController::class, 'update']);
+
+Route::delete('/delete/{id}',[App\Http\Controllers\EmployeeController::class, 'destroy']);
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::resource('customers','CustomerController')->except(['create','edit']);
